0b5fd4bb4e9f65d0b77d055124ea5fc469071725
ahmedadel2020-code <ahmedadelcode2024@gmail.com>
2021-12-29 06:18:15 +0200

    edit server.js

--- src/server/index.js
+++ src/server/index.js
@@ -23,8 +23,8 @@ app.use(express.static('dist'))
 
 
 app.get('/', function (req, res) {
-    // res.sendFile('dist/index.html')
-    res.sendFile(path.resolve('src/client/views/index.html'))
+    res.sendFile('dist/index.html')
+    // res.sendFile(path.resolve('src/client/views/index.html'))
 })
 
 /* 
